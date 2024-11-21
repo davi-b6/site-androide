3926144a1de715c119afa02440efb147f6fb9656
davi-b6 <davimartinsb6@gmail.com>
2024-11-21 14:13:12 -0300

    teste krz

--- script.js
+++
@@ -1,8 +0,0 @@
-// Seleciona elementos
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-
-// Adiciona evento de clique no botão hambúrguer
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-});
